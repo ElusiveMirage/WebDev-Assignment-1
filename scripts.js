0794ae44eefbdc4029876817bb20cb78cb4f96dd
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-10-14 11:19:36 +0800

    fixing map season 6

--- scripts.js
+++ scripts.js
@@ -12,8 +12,8 @@ function myFunction() {
 function myMap() {
   const singapore = { lat: 1.364106, lng: 103.930816 };
   const map = new google.maps.Map(document.getElementById("googleMap"), {
-    zoom: 15,
     center: singapore,
+    zoom: 15
   });
   const marker = new google.maps.Marker({position: singapore, map: map,});
 }
