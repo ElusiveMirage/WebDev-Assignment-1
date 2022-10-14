e85b7767bc8f6eb196a85b1717ce5854dca17751
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-10-14 11:30:31 +0800

    changing initial zoom level

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function myFunction() {
 function myMap() {
   const singapore = { lat: 1.364106, lng: 103.930816 };
   const map = new google.maps.Map(document.getElementById("googleMap"), {
-    zoom: 15,
+    zoom: 10,
     center: singapore
   });
   const marker = new google.maps.Marker({position: singapore, map: map,});
