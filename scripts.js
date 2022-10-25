8e1e0fa08ded52c2266af4e2840d72e4328ce6f4
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-10-25 21:02:46 +0800

    Added login form

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
 /* Place your JavaScript in this file */
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function topNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
+  }
+}
+
+function toggleLoginForm()
+{
+  var x = document.getElementsByClassName("login-container");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
