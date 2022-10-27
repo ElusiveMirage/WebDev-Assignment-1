3843851c4c76ae88144d0e22445ee5943b3309d9
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-10-27 18:55:12 +0800

    CSS changes, added jquery

--- scripts.js
+++ scripts.js
@@ -20,11 +20,33 @@ function toggleLoginForm()
   }
 }
 
+function toggleSignupForm()
+{
+  var x = document.getElementById("signup-container");
+
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
 function myMap() {
-  const singapore = { lat: 1.364106, lng: 103.930816 };
+  const singapore_loc_1 = { lat: 1.364106, lng: 103.930816 };
+  const singapore_loc_2 = { lat: 1.564106, lng: 105.930816 };
   const map = new google.maps.Map(document.getElementById("googleMap"), {
     zoom: 10,
     center: singapore
   });
-  const marker = new google.maps.Marker({position: singapore, map: map,});
+  const marker1 = new google.maps.Marker({position: singapore_loc_1, map: map,});
+  const marker2 = new google.maps.Marker({position: singapore_loc_2, map: map,});
 }
