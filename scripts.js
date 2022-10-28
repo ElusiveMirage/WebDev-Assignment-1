6b29a81817b1c495358373d522732e7d2c335bc4
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-10-28 23:56:06 +0800

    Added whole buncha crap

--- scripts.js
+++ scripts.js
@@ -82,3 +82,67 @@ function contactMap() {
   const marker1 = new google.maps.Marker({position: singapore_loc_1, map: map,});
   const marker2 = new google.maps.Marker({position: singapore_loc_2, map: map,});
 }
+
+function reservationMap() {
+  const singapore_loc_1 = { lat: 1.364106, lng: 103.930816 };
+  const singapore_loc_2 = { lat: 1.384106, lng: 103.920816 };
+  const map = new google.maps.Map(document.getElementById("reservation-googleMap"), {
+    zoom: 10,
+    center: singapore_loc_1
+  });
+  const marker1 = new google.maps.Marker({position: singapore_loc_1, map: map,});
+  const marker2 = new google.maps.Marker({position: singapore_loc_2, map: map,});
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#from-date");
+var checkoutElem = document.querySelector("#until-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
